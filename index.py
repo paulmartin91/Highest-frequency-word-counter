ae597d37883cf2f44a8fab4f9fc8dbd848a7882d
Paul Martin <paulmartin@MACBOOK176.local>
2020-08-09 14:05:34 +0100

    added user input for number of places returned

--- index.py
+++ index.py
@@ -1,19 +1,28 @@
-user_input = input("Please enter file name or directory: ")
-text_file_to_check = open(user_input)
+user_input_filename = input("Enter file name or directory: ")
+file_to_work_with = open(user_input_filename)
 
 word_histogram = dict()
 
-for line in text_file_to_check:
-    line_splitted_into_words = line.split()
-    for word in line_splitted_into_words:
+for line in file_to_work_with:
+    line_splitted_to_words = line.split()
+    for word in line_splitted_to_words:
         word_histogram[word] = word_histogram.get(word, 0) +1
 
-largest_current_word = dict()
-largest_current_word_count = 0
+word_histogram_reverse_values = list()
 
-for word,count in word_histogram.items():
-    if count > largest_current_word_count:
-        largest_current_word = {word: count}
-        largest_current_word_count = count
+for key, value in word_histogram.items():
+    word_histogram_reverse_values.append((value, key))
 
-print(largest_current_word)
+word_histogram_reverse_values_sorted = sorted(word_histogram_reverse_values, reverse=True)
+
+while True:
+    user_input_place_ammount = input("Number of places: ")
+    try:
+        int(user_input_place_ammount)
+        break
+    except:
+        print("Whole integers only please!")
+        continue
+
+for key, value in word_histogram_reverse_values_sorted[:int(user_input_place_ammount)]:
+    print(key, value)
